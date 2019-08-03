01bb6223f0f71dd5735465f7db52bb645fad8697
Roberto Souza <robertosouza57@yahoo.com.br>
2019-08-03 09:58:16 -0300

    primeiro commit

---
+++ src/main/java/com/nossossites/hievents/HieventsApplication.java
@@ -0,0 +1,13 @@
+package com.nossossites.hievents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HieventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HieventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nossossites/hievents/HieventsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nossossites.hievents;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HieventsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
